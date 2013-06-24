a616c9206340c8d4077730fc3eb07e59c46e93e7
miltonlab <1000ton.lab@gmail.com>
2013-06-24 09:26:18 -0500

    iniciando repositorio con configuraciones del SGAUNL

---
+++ sga.sh
@@ -0,0 +1,4 @@
+alias cdsga='cd ~/ddsunl/sga/sgacode/sga-privado/sga/'
+alias envsga='source ~/ddsunl/sga/sgaenv/bin/activate'
+alias shsga='envsga; cdsga; tg-admin shell'
+alias runsga='envsga;cdsga; python start-sga.py'
